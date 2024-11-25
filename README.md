4f9506556ea73aeae655511757ead06ff1b5e4ce
Fidel Zarco <fidel.zarco1608@gmail.com>
2024-11-25 17:54:51 -0600

    Modificación en README y Seeder

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 
 ## Instrucciones para desplegar
 
+### Composer
+ - Ejecutar ```composer install ```
+
+### Base de datos y .env
+ - Crear el schema en MYSQL
+ - Copiar y pegar el archivo .env.example y renombrarlo a ```.env ```
+ - Dentro del archivo ```.env ``` modificar el campo ```DB_DATABASE ``` con el nombre del schema que se creo, modificar el campo ```DB_USERNAME ``` con el nombre del superusuario de la base de datos, modificar el campo ```DB_PASSWORD ``` con la contraseña del superusuario de la base de datos
+  - Ejecutar ```php artisan migrate```
+
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Rol;
+use App\Models\RolUsuario;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsuariosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+
+        //Usuarios
+        User::factory(15)->create();
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => '12345'
+        ]);
+        User::create([
+            'name' => 'user',
+            'email' => 'user@example.com',
+            'password' => '12345'
+        ]);
+        //Roles
+        Rol::create([
+            'name' => 'Administrador',
+            'slug' => 'administrador' ,
+            'description' => now(),
+            'created_at' => now()
+        ]);
+        Rol::create([
+            'name' => 'Usuario',
+            'slug' => 'usuario' ,
+            'description' => now(),
+            'created_at' => now()
+        ]);
+
+        //Admin
+        RolUsuario::create([
+            'user_id' => 'admin@example.com',
+            'role_id' => 1,
+            'created_at' => now()
+        ]);
+        //Usuario
+        RolUsuario::create([
+            'user_id' => 'user@example.com',
+            'role_id' => 2,
+            'created_at' => now()
+        ]);
+
+
+    }
+}
